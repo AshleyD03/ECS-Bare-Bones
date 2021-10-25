6e42a9081b11aa1edfa46ac8627baf8cc14c32fb
Ashley <adoel3507@gmail.com>
2021-10-25 23:33:45 +0100

    Changed from Recursive read to while loop. Added Stack for scope storage & LoopCondition for storing while conditions

--- Interpreter.java
+++ Interpreter.java
@@ -4,36 +4,103 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.ArrayList;
 
 public class Interpreter {
 
   /**
    * Main.
+   * 
    * @param args
    */
   public static void main(String[] args) {
+
     // Take file path
     String path = input("What file do you want to run?");
-  
-    // Read File as a list
-    List<String> file = null;
+
+    // Read file as list of strings
+    List<String> lines = null;
     try {
-      file = loadFile(path);
+      lines = loadFile(path);
     } catch (IOException e) {
-      System.err.println("Error caught with load-file");
-      System.err.println(e);
+      System.err.println("Error caught with load-file" + e);
       System.exit(0);
     }
 
-    // Start recursive line reads from top
-    readLines(file);
-    System.out.println("Complete :D");
+    // Init lineNum, loopList and stack
+    Integer lineNum = 0;
+    Stack stack = new Stack();
+    ArrayList<LoopCondition> loops = new ArrayList<LoopCondition>();
+
+    // Begin loop through lines
+    while (lineNum < lines.size()) {
+
+      // Interp line
+      String line = lines.get(lineNum);
+      String[] fields = interpLine(line);
+
+      System.out.println(Arrays.toString(fields));
+
+      // Perform lines command from first field
+      switch (fields[0]) {
+      case "clear":
+        stack.init(fields[1], 0);
+        break;
+
+      case "incr":
+        Integer inc = stack.read(fields[1]) + 1;
+        stack.write(fields[1], inc);
+        break;
+
+      case "decr":
+        Integer dec = stack.read(fields[1]) - 1;
+        stack.write(fields[1], dec);
+        break;
+
+      case "while":
+        LoopCondition newLoop = new LoopCondition(fields[1], fields[3], fields[2], lineNum);
+        if (newLoop.isTrue(stack)) {
+          loops.add(newLoop);
+          stack.addScope();
+        }
+        break;
+
+      case "end":
+        LoopCondition curLoop = loops.get(loops.size() - 1);
+        if (curLoop.isTrue(stack)) {
+          lineNum = curLoop.lineNum;
+        } else {
+          loops.remove(loops.size() - 1);
+          stack.removeScope();
+        }
+        break;
+
+      case "print":
+        System.out.println(stack.read(fields[1]) + "\n");
+        break;
+      }
+      ;
+      lineNum++;
+      stack.print();
+    }
+    ;
+  };
+
+  /**
+   * Breaks down a line into all it's fields Method from :
+   * https://stackoverflow.com/questions/2932392/java-how-to-replace-2-or-more-spaces-with-single-space-in-string-and-delete-lead
+   * 
+   * @param line
+   * @return
+   */
+  public static String[] interpLine(String line) {
+    line = line.trim().replaceAll(" +", " ");
+    return line.split(" ");
   }
 
   /**
    * loads file from directory.
+   * 
    * @param path String.
    * @return List<String>.
    * @throws IOException
@@ -44,59 +111,9 @@ public class Interpreter {
     return list;
   }
 
-  public static void readLines(List<String> lines) {
-
-    HashMap<String, Integer> heap = new HashMap<String, Integer>();
-
-    for(int i=0; i < lines.size() - 1; i++) {
-
-      String line = lines.get(i);
-      String[] fields = abstractLine(line);
-
-      switch (fields[0]) {
-        case "clear":
-          heap.put(fields[1], 0);
-          break;
-
-        case "incr":
-          heap.put(fields[1], heap.get(fields[1]) + 1);
-          break;
-
-        case "decr":
-          heap.put(fields[1], heap.get(fields[1]) - 1);
-          break;
-
-        case "while":
-          List<String> loopList = new ArrayList<String>();
-          int x = i;
-          while (abstractLine(lines.get(x + 1))[0] != "break") {
-            loopList.add(lines.get(x));
-            x++;
-          }
-          // Add Condition
-          while ( false ) readLines(loopList);
-          break;
-
-        case "print":
-          System.out.println(heap.get(fields[1]) + "\n");
-          break;
-      }
-    }
-  }
-
-  /**
-   * Breaks down a line into all it's fields
-   * @param line
-   * @return
-   */
-  public static String[] abstractLine (String line) {
-    // https://stackoverflow.com/questions/2932392/java-how-to-replace-2-or-more-spaces-with-single-space-in-string-and-delete-lead
-    line = line.trim().replaceAll(" +", " ");
-    return line.split(" ");
-  }
-
   /**
    * Python input function.
+   * 
    * @param msg
    * @return
    */
